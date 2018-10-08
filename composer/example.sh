a24b3d9186f7fbd3cd53feb31cf5c44b1d3e9c66
Diego Martin <marg@zhaw.ch>
2018-10-08 13:54:52 +0200

    [A] Composer/example.sh

---
+++ composer/example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# The following script handles the deploy of the example app into the IBM
+# Bluemix platform.
+#
+# Be aware that the script assumes that you have already configured the IBM
+# Cloud CLI by yourself with your credentials. If that's not the case remember
+# to issue the following line before executing this script:
+#
+#   $> bx login
+#
+# Author: Diego Martin (October 2018)
+#
+################################################################################
