82d951e5359fcdbc798f95cc93f03d2d7fba7136
Diego Martin <marg@zhaw.ch>
2018-10-08 13:55:31 +0200

    [A] Fission/example.sh

---
+++ fission/example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# The following script handles the deploy of the example app within the Fission
+# Workflows already deployed.
+#
+# Be aware that the script assumes that you have a proper Fission Workflows
+# already in place before executing it.
+#
+# Author: Diego Martin (October 2018)
+#
+################################################################################
