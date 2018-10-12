d8b8c144a47f3dfc6f9c92f30f1086e331421572
Diego Martin <marg@zhaw.ch>
2018-10-12 17:49:42 +0200

    [A] src/addTodo.js

---
+++ src/addTodo.js
@@ -0,0 +1,18 @@
+// Function: addTodo
+//
+// The following function adds the new ToDo under the ToDos-tag.
+//
+// Author: Diego Martin (October 2018)
+//
+////////////////////////////////////////////////////////////////////////////////
+
+function main(params) {
+
+    var data = params.data;
+
+    data.todo[params.data.count] = params.action.add;
+
+    data.count = parseInt(params.data.count, 10) + 1;
+
+    return { "data": data };
+};
