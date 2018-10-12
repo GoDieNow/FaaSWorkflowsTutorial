49417b9b5fecb7363e8d21cb04c3a9918f998cca
Diego Martin <marg@zhaw.ch>
2018-10-12 17:51:10 +0200

    [A] src/wipTodo.js

---
+++ src/wipTodo.js
@@ -0,0 +1,36 @@
+// Function: wipTodo
+//
+// The following function moves the ToDo selected under the WiP-tag.
+//
+// Author: Diego Martin (October 2018)
+//
+////////////////////////////////////////////////////////////////////////////////
+
+function main(params) {
+
+    var data = params.data;
+
+    var todo = "";
+
+    if (data.todo[params.action.wip]) {
+
+        todo = data.todo[params.action.wip];
+        delete data.todo[params.action.wip];
+
+    } else if (data.done[params.action.wip]) {
+
+        todo = data.done[params.action.wip];
+        delete data.done[params.action.wip];
+
+    } else {
+
+        return params;
+
+    }
+
+    data.wip[params.action.wip] = todo;
+
+    return Object.assign(params,
+        { "data": data }
+    );
+};
