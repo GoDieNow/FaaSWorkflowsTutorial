7a091ac9dd9a0a6fe4b850be0d3fc6ff40b0040e
Diego Martin <marg@zhaw.ch>
2018-10-12 17:53:09 +0200

    [A] src/composer_todosAppComposition.js

---
+++ src/composer_todosAppComposition.js
@@ -0,0 +1,26 @@
+//
+// The following piece of code describes the composition of functions needed
+// by IBM Composer to create the app for the example.
+//
+// Author: Diego Martin (October 2018)
+//
+////////////////////////////////////////////////////////////////////////////////
+
+composer.sequence('FWTt/getTodos',
+    composer.if(params => params.action,
+        composer.sequence(
+            composer.if(params => params.action.add || params.action.del,
+                composer.if(params => params.action.add,
+                    'FWTt/addTodo',
+                    'FWTt/delTodo'
+                ),
+                composer.if(params => params.action.wip,
+                    'FWTt/wipTodo',
+                    'FWTt/doneTodo'
+                )
+            ),
+            'FWTt/updateTodos'
+        ),
+    ),
+    'FWTt/showTodos'
+);
