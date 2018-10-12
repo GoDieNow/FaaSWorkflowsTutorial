4d338a4d976f6f60a7ab698f091f6f26c9bc4353
Diego Martin <marg@zhaw.ch>
2018-10-12 17:52:26 +0200

    [A] src/showTodos.js

---
+++ src/showTodos.js
@@ -0,0 +1,14 @@
+// Function: showTodos
+//
+// The following function sends the state of the previous operation (if
+// available) and a copy of the actual data.
+//
+// Author: Diego Martin (October 2018)
+//
+////////////////////////////////////////////////////////////////////////////////
+
+function main(params) {
+
+    return params;
+
+};
