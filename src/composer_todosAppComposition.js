e7baaaac139d01546ffdaabda5cbffb305f973c9
Diego Martin <marg@zhaw.ch>
2018-10-15 15:39:18 +0200

    [F] src/composer_todosAppComposition.js
    Composition JS not translating a JSON-Object as a "true-value" in a if-statement
    Added composer.empty() for explicitness on the if-statement

--- src/composer_todosAppComposition.js
+++ src/composer_todosAppComposition.js
@@ -7,7 +7,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 composer.sequence('FWTt/getTodos',
-    composer.if(params => params.action,
+    composer.if(params => (!!params.action) && (params.action.constructor === Object),
         composer.sequence(
             composer.if(params => params.action.add || params.action.del,
                 composer.if(params => params.action.add,
@@ -21,6 +21,7 @@ composer.sequence('FWTt/getTodos',
             ),
             'FWTt/updateTodos'
         ),
+        composer.empty()
     ),
     'FWTt/showTodos'
 );
