2280a4659eab082cad1e690f5a04eed51a912b83
Diego Martin <marg@zhaw.ch>
2018-10-15 15:33:41 +0200

    [F] src/addTodo.js
    Preventing the case of empty "todo" entry in the JSON data-set

--- src/addTodo.js
+++ src/addTodo.js
@@ -10,6 +10,10 @@ function main(params) {
 
     var data = params.data;
 
+    if (!data.todo) {
+        data.todo = {};
+    }
+
     data.todo[params.data.count] = params.action.add;
 
     data.count = parseInt(params.data.count, 10) + 1;
