6c0c29e04706bc194d92cfa0a00119e989b29bbd
Diego Martin <marg@zhaw.ch>
2018-10-18 12:00:35 +0200

    [U] Composer/example.sh
    Added the environment creation and deployment of functions

--- composer/example.sh
+++ composer/example.sh
@@ -29,4 +29,71 @@
 source ../src/front_functions.sh
 
 
+# Preparations
 #
+# Before start and in case this is not the first time running the example we
+# will make a clean-up of the functions in the Bluemix and also we will reset
+# the functions (in case this is done after using the Fission example) and
+# then recover the bin that we have alreasy got...
+#
+################################################################################
+
+echo "$alert > Let's start with the preparations for the example...$reset"
+
+# Let's clean first
+echo "$alert > Let's start with the cleanup...$reset"
+for i in "${srcFiles[@]}"
+do
+	fsh wsk action delete FWTt/$i
+done
+fsh app delete FWTt/simpleTodos
+
+# Now we call the restore function..
+resetWithBin()
+
+# Enviroment
+#
+# Now we will deploy the functions and inspect the workflow through the fsh
+# shell preview utility.
+#
+################################################################################
+
+# Let's deploy the functions
+echo "$alert > Let's deploy the functions...$reset"
+for i in "${srcFiles[@]}"
+do
+	fsh wsk action create FWTt/$i $gitdir/src/$i.js
+done
+
+echo "$alert > Now we create the composition/workflow...$reset"
+# Now we create the composition
+fsh app create FWTt/simpleTodos $gitdir/src/composer_todosAppComposition.js
+
+echo "$alert > And after that we expose it...$reset"
+# Now we expose it
+fsh webbify FWTt/simpleTodos
+
+echo "$alert > Let's check how that composition looks like...$reset"
+# Let's see how the composition looks like..
+fsh app preview $gitdir/src/composer_todosAppComposition.js
+
+# A break-point..
+read -s
+
+# Now we are going to reset to a known state the bin and then check that
+# everything has been setted up properly :)
+echo "$alert > Let's reset the bin to a known-state...$reset"
+fsh wsk action invoke FWTt/updateTodos
+echo "$alert > And now let's check the composition is working...$reset"
+curl "https://openwhisk.eu-gb.bluemix.net/api/v1/web/${USER}_${SPACE}/FWTt/simpleTodos.json"
+
+# A break-point..
+read -s
+
+
+# Front-end
+################################################################################
+
+echo "$alert > Since everything is in place, let's start the front-end...$reset"
+# Now let's the fun begin! :D
+cliTodosApp(composer)
